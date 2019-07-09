9021148d9b93a65fb545acc9f3f7311384893e5b
ishan <ishanshukla97@gmail.com>
2019-07-09 17:22:50 +0530

    chore: persist state on refresh

--- admin-frontend/src/index.js
+++ admin-frontend/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
-import reduxThunk from 'redux-thunk'
+import reduxThunk from "redux-thunk";
+import { loadState, saveState } from "./utils/localStorage";
+import throttle from "lodash/throttle";
 import "materialize-css/dist/css/materialize.min.css";
 
 import "./index.css";
 import App from "./App";
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const persistedState = loadState();
+const store = createStore(
+	reducers,
+	persistedState,
+	applyMiddleware(reduxThunk)
+);
+
+store.subscribe(
+	throttle(() => {
+		saveState(store.getState());
+	}, 1000)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
--- member-frontend/src/index.js
+++ member-frontend/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
+import { loadState, saveState } from "./utils/localStorage.js"
 import App from "./App";
 import reducers from "./services/reducers";
 import "materialize-css/dist/css/materialize.min.css";
+import throttle from "lodash/throttle"
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const persistedState = loadState()
+console.log(persistedState)
+const store = createStore(reducers, persistedState, applyMiddleware(reduxThunk));
+
+store.subscribe(
+	throttle(() => {
+		saveState(store.getState());
+	}, 1000)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
