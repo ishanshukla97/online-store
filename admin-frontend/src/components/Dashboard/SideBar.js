69753bfaea759f4b06aa29b69d816fc37a33c845
ishan <ishanshukla97@gmail.com>
2019-07-13 13:59:28 +0530

    chore: add subscriptions and fix Sidebar actions import error

--- admin-frontend/src/components/Dashboard/SideBar.js
+++ admin-frontend/src/components/Dashboard/SideBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as actions from "../../actions";
+import * as actions from "../../services/view/actions";
 
 const SideBar = props => {
 	const ITEMS = ["Products", "Orders", "Reviews"];
--- admin-frontend/src/index.js
+++ admin-frontend/src/index.js
@@ -4,20 +4,52 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./services/reducers";
 import reduxThunk from "redux-thunk";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 import { loadState, saveState } from "./utils/localStorage";
 import throttle from "lodash/throttle";
 import "materialize-css/dist/css/materialize.min.css";
-
+import { HTTP_URL, WS_URL } from "./utils/constants";
 import "./index.css";
 import App from "./App";
-
-const client = new ApolloClient({
-	uri: "http://localhost:5000/admin/graphql"
-});
+import { ApolloProvider } from "react-apollo";
+// Remove the apollo-boost import and change to this:
+import ApolloClient from "apollo-client";
+// Setup the network "links"
+import { WebSocketLink } from "apollo-link-ws";
+import { HttpLink } from "apollo-link-http";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 const persistedState = loadState();
+console.log(persistedState);
+const wsLink = new WebSocketLink({
+	uri: WS_URL,
+	options: {
+		reconnect: true,
+		connectionParams: {
+			authToken: persistedState
+		}
+	}
+});
+const httpLink = new HttpLink({
+	uri: HTTP_URL
+});
+
+const link = split(
+	// split based on operation type
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query);
+		return kind === "OperationDefinition" && operation === "subscription";
+	},
+	wsLink,
+	httpLink
+);
+
+const client = new ApolloClient({
+	link,
+	cache: new InMemoryCache()
+});
+
 const store = createStore(
 	reducers,
 	persistedState,
