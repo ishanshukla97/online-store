86b7def4ce3313f5129a6e22d71d9c93bf2f70d2
ishan <ishanshukla97@gmail.com>
2019-08-05 01:05:31 +0530

    fix: add env aware urls

--- admin-frontend/src/utils/constants.js
+++ admin-frontend/src/utils/constants.js
@@ -1,5 +1,12 @@
-export const HTTP_URL = "http://localhost:5000/admin/graphql";
-export const WS_URL = "ws://localhost:5000/graphql";
+
+if (process.env.NODE_ENV === 'production'){
+	export const HTTP_URL = "https://" + process.env.BACKEND_URL + "/admin/graphql";
+	export const WS_URL = "ws://" + process.env.BACKEND_URL + "localhost:5000/graphql";
+} else {
+	export const HTTP_URL = "http://localhost:5000/admin/graphql";
+	export const WS_URL = "ws://localhost:5000/graphql";
+
+}
 export const ORDER_STATUS = {
 	CANCELED: "CANCELED",
 	PENDING: "PENDING",
--- member-frontend/src/utils/constants.js
+++ member-frontend/src/utils/constants.js
@@ -1,1 +1,7 @@
-export const HTTP_URL = "http://localhost:5000/member/graphql";
+
+if (process.env.NODE_ENV === 'production'){
+	export const HTTP_URL = "https://" + process.env.BACKEND_URL + "/admin/graphql";
+} else {
+	export const HTTP_URL = "http://localhost:5000/admin/graphql";
+
+}
