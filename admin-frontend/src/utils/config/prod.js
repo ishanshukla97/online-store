6211eedaa8a2007891944c33dd17c37c5d1c914d
ishan <ishanshukla97@gmail.com>
2019-08-05 01:27:46 +0530

    fix: wss error

--- admin-frontend/src/utils/config/prod.js
+++ admin-frontend/src/utils/config/prod.js
@@ -1,2 +1,2 @@
 export const HTTP_URL = "https://" + process.env.BACKEND_URL + "/admin/graphql";
-export const WS_URL = "ws://" + process.env.BACKEND_URL + "localhost:5000/graphql";
+export const WS_URL = "wss://" + process.env.BACKEND_URL + "localhost:5000/graphql";
